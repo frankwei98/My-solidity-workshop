f35a49e5157112493fb360037bfde04948f12ed3
Frank Wei <frank@frankwei.xyz>
2020-11-11 12:04:47 +0800

    go

--- deployment/PeggedFactory.ts
+++ deployment/PeggedFactory.ts
@@ -4,25 +4,31 @@ require("dotenv").config();
 
 // Importing related complied contract code
 import MatatakiPeggedTokenFactory from "../build/MatatakiPeggedTokenFactory.json";
+import { BinanceSmartChain } from "./networks";
 
 // Use the Rinkeby testnet
-const network = "rinkeby";
+const network = BinanceSmartChain.TESTNET;
 // Specify your own API keys
 // Each is optional, and if you omit it the default
 // API key for that service will be used.
-const provider = ethers.getDefaultProvider(network, {
-  infura: process.env.InfuraId,
+const provider = new ethers.providers.JsonRpcProvider(network.rpcUrl, {
+  name: "BSC Testnet",
+  chainId: network.chainId,
 });
 
 const wallet = new ethers.Wallet(String(process.env.privateKey), provider);
 
 async function main() {
-  const router02 = await ethers.ContractFactory.fromSolidity(
+  const blackList = "0xBD98FB43Ea8Cfc94D4fd0Fe61BcE98B64579dc55";
+  const factory = await ethers.ContractFactory.fromSolidity(
     MatatakiPeggedTokenFactory,
     wallet
   ).deploy();
-  const router02Receipt = await router02.deployTransaction.wait(1);
-  console.info(`Router02: ${router02Receipt.contractAddress}`);
+  const deployReceipt = await factory.deployTransaction.wait(1);
+  console.info(`Factory deployed at: ${deployReceipt.contractAddress}`);
+  const deployed = await factory._deployed();
+  await deployed.initBlacklistManager(blackList);
+  console.info("initized BlacklistManager");
 }
 
 main();
---
+++ deployment/networks.ts
@@ -0,0 +1,14 @@
+
+
+export const BinanceSmartChain = {
+    MAINNET: {
+        rpcUrl: 'https://bsc-dataseed.binance.org/',
+        chainId: 56,
+        platformCurrency: 'BNB'
+    },
+    TESTNET: {
+        rpcUrl: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
+        chainId: 97,
+        platformCurrency: 'BNB'
+    }
+}
