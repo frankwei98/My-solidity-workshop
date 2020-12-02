f5ae492a9a1a6f0b50a298aa44c2c2a703788e7a
Frank Wei <frank@frankwei.xyz>
2020-12-02 15:25:01 +0800

    feat: safer burner

--- src/TokenBurner.sol
+++ src/TokenBurner.sol
@@ -23,14 +23,43 @@ interface Burnable {
     function burn(address account, uint256 amount) external;
 }
 
-contract TokenBurner {
+interface IMatatakiPeggedTokenFactory {
+    function allPeggedTokens() external returns(address[] memory);
+    function symbolToAddress(string calldata) external returns(address);
+    function computeAddress(string calldata _name, string calldata _symbol)
+        external
+        view
+        returns (address predictedAddress);
+}
+
+interface IERC20Detailed {
+    function name() external returns(string memory);
+    function symbol() external returns(string memory);
+}
+
+contract MatatakiTokenBurner {
+    address public factory;
     event BurnFanPiao(address indexed tokenAddress, uint256 uid, uint256 value);
+
+    constructor(address _factory) public {
+        factory = _factory;
+    }
+
+    modifier isMatatakiPeggedToken(address _token) {
+        IERC20Detailed erc20 = IERC20Detailed(_token);
+        string memory name = erc20.name();
+        string memory symbol = erc20.symbol();
+        address computed = IMatatakiPeggedTokenFactory(factory).computeAddress(name, symbol);
+        address registered = IMatatakiPeggedTokenFactory(factory).symbolToAddress(symbol);
+        require(registered != address(0) && computed == registered, "MatatakiTokenBurner::BAD_TOKEN: Failed to lookup");
+        _;
+    }
 
     function burn(
         address token,
         uint256 uid,
         uint256 value
-    ) public {
+    ) public isMatatakiPeggedToken(token) {
         IERC20(token).transferFrom(msg.sender, address(this), value); // send value to us
         // burn value in our contract, the contract have to to be admin
         Burnable(token).burn(address(this), value);
